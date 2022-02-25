3b60f19b1720469200300c5b7b9ea8879f8d519c
Lucy Cifferello <12953208+elvece@users.noreply.github.com>
2022-02-25 13:00:10 -0700

    check for mountpoint existence on startup

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -6,5 +6,22 @@ export RTL_CONFIG_PATH=/root
 echo start9/public > .backupignore
 echo start9/shared >> .backupignore
 
+if ! test -d /mnt/lnd then
+  echo "LND mountpoint does not exist"
+  exit 0
+fi
+
+while ! test -f /mnt/lnd/tls.cert
+do
+    echo "Waiting for LND cert to be generated..."
+    sleep 1
+done
+
+while ! test -f /mnt/lnd/admin.macaroon
+do
+  echo "Waiting for LND admin macaroon to be generated..."
+  sleep 1 
+done
+
 configurator
 exec tini -g -- node rtl
