5019c0f9ccdfafc330628e5c89370d998f56a76b
Lucy Cifferello <12953208+elvece@users.noreply.github.com>
2022-02-25 18:48:37 -0700

    update copy and remove unecessary keys

--- check-web.sh
+++ check-web.sh
@@ -7,7 +7,7 @@ else
     curl --silent --fail ride-the-lightning.embassy &>/dev/null
     exit_code=$?
     if test "$exit_code" != 0; then
-        echo "Web interface is unreachable" >&2
+        echo "The RTL UI is unreachable" >&2
         exit 1
     fi
 fi
