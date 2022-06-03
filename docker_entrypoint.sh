4f8f4ec70b8f99522deafe8308b258743408bd4c
Chris Guida <chrisguida@gmail.com>
2022-06-02 19:30:10 -0500

    allow running without LND installed, change deps to opt-in, add null migration

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -8,7 +8,7 @@ rm -rf /root/cl-external-* /root/lnd-external-*
 echo start9/public > /root/.backupignore
 echo start9/shared >> /root/.backupignore
 
-result=$(yq '.nodes.[] | select(.connection-settings.type == "internal")' /root/start9/config.yaml)
+result=$(yq '.nodes.[] | select(.connection-settings.type == "internal" and .type == "lnd")' /root/start9/config.yaml)
 if [[ ! -z $result ]]
   then
 
@@ -21,7 +21,24 @@ if [[ ! -z $result ]]
   while ! test -f /mnt/lnd/admin.macaroon
   do
     echo "Waiting for LND admin macaroon to be generated..."
-    sleep 1 
+    sleep 1
+  done
+fi
+
+result=$(yq '.nodes.[] | select(.connection-settings.type == "internal" and .type == "c-lightning")' /root/start9/config.yaml)
+if [[ ! -z $result ]]
+  then
+
+  if ! test -d /mnt/c-lightning
+  then
+    echo "Core Lightning mountpoint does not exist"
+    exit 0
+  fi
+
+  while ! test -f /mnt/c-lightning/access.macaroon
+  do
+    echo "Waiting for c-Lightning-REST access macaroon to be generated..."
+    sleep 1
   done
 fi
 
