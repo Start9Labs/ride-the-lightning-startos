c82833d29db17263d3bba30274e9101ec2ace045
Dread <dread@IslandBitcoin.attlocal.net>
2022-07-08 12:30:03 -0400

    add matches type to js fix

--- scripts/services/setConfig.ts
+++ scripts/services/setConfig.ts
@@ -1,11 +1,13 @@
-import { compat, types as T } from "../deps.ts";
+import { matches, compat, types as T } from "../deps.ts";
+const {shape, literal} = matches
+const matchIsRest = shape({advanced:shape({plugin: shape({rest: literal(true)})})}).test
 
 // deno-lint-ignore require-await
 export const setConfig: T.ExpectedExports.setConfig = async (
   effects: T.Effects,
   newConfig: T.Config,
 ) => {
-  const dependsOnCln: { [key: string]: string[] } = (newConfig as any).advanced.plugins.rest ? { "c-lightning": [] } : {};
+  const dependsOnCln: { [key: string]: string[] } = matchIsRest(newConfig) ? { "c-lightning": [] } : {};
   return compat.setConfig(effects, newConfig, {
     ...dependsOnCln,
   });
