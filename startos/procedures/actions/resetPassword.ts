baf8e2108593d509b287b97fe724144627ae8d50
Matt Hill <matthewonthemoon@gmail.com>
2023-05-23 10:30:22 -0600

    utils vault

--- startos/procedures/actions/resetPassword.ts
+++ startos/procedures/actions/resetPassword.ts
@@ -47,7 +47,7 @@ export const resetPassword = sdk.createAction(
     await rtlConfig.write(config, effects)
 
     // Save password to vault
-    await effects.vault.set({ key: 'password', value: password })
+    await utils.vault.set('password', password)
 
     return {
       message: 'Password changed successfully and saved to your Vault.',
--- startos/procedures/init.ts
+++ startos/procedures/init.ts
@@ -9,7 +9,7 @@ const install = sdk.setupInstall(async ({ effects, utils }) => {
   // generate random password
   const password = getDefaultString(randomPassword)
   // Save password to vault
-  await effects.vault.set({ key: 'password', value: password })
+  await utils.vault.set('password', password)
   // create RTL config file with defaults
   await rtlConfig.write(
     {
