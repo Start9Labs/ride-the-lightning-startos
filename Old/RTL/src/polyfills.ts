12155cda5c55e2bc1aa80696beae17475ca543ab
Matt Hill <mattnine@protonmail.com>
2025-04-21 22:18:36 -0600

    remove old dir

--- Old/RTL/src/polyfills.ts
+++
@@ -1,3 +0,0 @@
-window.global = <any>window;
-window.global.Buffer = window.global.Buffer || require('buffer').Buffer;
-window.global.process = window.global.process || require('process');
--- Old/scripts/deps.ts
+++
@@ -1,1 +0,0 @@
-export * from "https://deno.land/x/embassyd_sdk@v0.3.3.0.9/mod.ts";
