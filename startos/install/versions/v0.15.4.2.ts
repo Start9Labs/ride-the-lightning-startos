d803c2084e5d3ab3613eb9398fe6c57a4729f780
Shadowy Super Coder <musashidisciple@proton.me>
2025-08-20 16:52:31 -0600

    bump pre-release tag

--- startos/install/versions/v0.15.4.2.ts
+++ startos/install/versions/v0.15.4.2.ts
@@ -4,30 +4,31 @@ import { rtlConfig } from '../../file-models/RTL-Config.json'
 import { clnMountpoint, lndMountpoint } from '../../utils'
 
 export const v_0_15_4_2 = VersionInfo.of({
-  version: '0.15.4-beta:2-alpha.1',
+  version: '0.15.4-beta:2-alpha.2',
   releaseNotes: 'Updated for StartOS v0.4.0',
   migrations: {
     up: async ({ effects }) => {
       const config = await rtlConfig.read().once()
 
       await rtlConfig.merge(effects, {
-        nodes: config?.nodes?.map((n, index) => {
-          if (n.settings.lnServerUrl.includes('lnd.embassy')) {
-            n.settings.lnServerUrl = 'https://lnd.startos:8080'
-            n.lnNode = 'Internal-LND'
-            n.index = 1
-            n.authentication.macaroonPath = lndMountpoint
-          } else if (n.settings.lnServerUrl.includes('c-lightning.embassy')) {
-            n.settings.lnServerUrl = 'https://c-lightning.startos:8080'
-            n.lnNode = 'Internal-CLN'
-            n.index = 2
-            n.authentication.runePath = clnMountpoint
-          } else {
-            n.index = index + 2
-          }
+        nodes:
+          config?.nodes?.map((n, index) => {
+            if (n.settings.lnServerUrl.includes('lnd.embassy')) {
+              n.settings.lnServerUrl = 'https://lnd.startos:8080'
+              n.lnNode = 'Internal-LND'
+              n.index = 1
+              n.authentication.macaroonPath = lndMountpoint
+            } else if (n.settings.lnServerUrl.includes('c-lightning.embassy')) {
+              n.settings.lnServerUrl = 'https://c-lightning.startos:8080'
+              n.lnNode = 'Internal-CLN'
+              n.index = 2
+              n.authentication.runePath = clnMountpoint
+            } else {
+              n.index = index + 2
+            }
 
-          return n
-        }) || [],
+            return n
+          }) || [],
       })
 
       await Promise.all([
