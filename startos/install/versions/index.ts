24ca48a4c6c779c682ce3800b7851041a4bb6ca0
Shadowy Super Coder <musashidisciple@proton.me>
2025-09-24 09:57:04 -0600

    increment pre-release to beta.0

--- startos/install/versions/index.ts
+++ startos/install/versions/index.ts
@@ -1,2 +1,4 @@
-export { v_0_15_5_1 as current } from './v0.15.5.1'
-export const other = []
+export { v_0_15_5_1_beta0 as current } from './v0.15.5.1-beta.1-beta.0'
+import { v_0_15_5_1 } from './v0.15.5.1'
+
+export const other = [v_0_15_5_1]
