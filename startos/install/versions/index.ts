56177477198eacc6181facc16f6faba72da10edc
Shadowy Super Coder <musashidisciple@proton.me>
2025-11-26 10:42:14 -0700

    bump pre-release

--- startos/install/versions/index.ts
+++ startos/install/versions/index.ts
@@ -1,4 +1,4 @@
-export { v_0_15_5_1_beta1 as current } from './v0.15.5.1-beta.1-beta.1'
+export { v_0_15_5_1_beta2 as current } from './v0.15.5.1-beta.1-beta.2'
 import { v_0_15_5_1 } from './v0.15.5.1'
 
 export const other = [v_0_15_5_1]
--- startos/install/versions/v0.15.5.1-beta.1-beta.2.ts
+++ startos/install/versions/v0.15.5.1-beta.1-beta.2.ts
@@ -1,7 +1,7 @@
 import { VersionInfo, IMPOSSIBLE } from '@start9labs/start-sdk'
 
-export const v_0_15_5_1_beta1 = VersionInfo.of({
-  version: '0.15.5-beta:1-beta.1',
+export const v_0_15_5_1_beta2 = VersionInfo.of({
+  version: '0.15.5-beta:1-beta.2',
   releaseNotes: 'Updated for StartOS v0.4.0',
   migrations: {
     down: IMPOSSIBLE,
